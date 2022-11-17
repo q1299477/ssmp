db050b2a8517623d05f2bf93f0ee96dcd963a59f
ch <1299477181@qq.com>
2022-11-17 16:45:33 +0800

    second Commit

--- src/main/java/com/ch/ssmp/controller/BookController.java
+++ src/main/java/com/ch/ssmp/controller/BookController.java
@@ -15,28 +15,34 @@ public class BookController {
 
     @Autowired
     private IBookService bookService;
+
     @GetMapping
-    public List<Book> getAll(){
+    public List<Book> getAll() {
         return bookService.list();
     }
+
     @PostMapping
-    public Boolean save(@RequestBody Book book){
+    public Boolean save(@RequestBody Book book) {
         return bookService.save(book);
     }
+
     @PutMapping
-    public Boolean update(@RequestBody Book book){
+    public Boolean update(@RequestBody Book book) {
         return bookService.modify(book);
     }
+
     @DeleteMapping("{id}")
-    public Boolean delete(@PathVariable Integer id){
+    public Boolean delete(@PathVariable Integer id) {
         return bookService.delete(id);
     }
+
     @GetMapping("{id}")
-    public Book getById(@PathVariable Integer id){
+    public Book getById(@PathVariable Integer id) {
         return bookService.getById(id);
     }
+
     @GetMapping("{currentPage}/{pageSize}")
-    public IPage<Book> getPage(@PathVariable int currentPage,@PathVariable int pageSize){
-        return bookService.getpage(currentPage,pageSize);
+    public IPage<Book> getPage(@PathVariable int currentPage, @PathVariable int pageSize) {
+        return bookService.getpage(currentPage, pageSize);
     }
 }
